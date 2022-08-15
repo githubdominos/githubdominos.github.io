fa652a737ab0e38d2defd3c0ce19e8b4b30733ab
Vitor Alecrim <vitor.alecrim.p@gmail.com>
2022-08-15 15:05:19 -0300

    menu link added

--- data.js
+++ data.js
@@ -3,10 +3,10 @@ const links = [
        name:"ligue e peça agora!",
        link:'tel:+557135992000',
      },
-    //  {
-    //    name:"cardápio aqui",
-    //    link:"https://drive.google.com/drive/folders/1UCyd9O0P6C_yobzsWz-e2D40xx-rcl8Y"
-    //  }, 
+     {
+       name:"cardápio aqui",
+       link:"https://drive.google.com/drive/folders/1UCyd9O0P6C_yobzsWz-e2D40xx-rcl8Y"
+     }, 
      {
        name:"pedidos ifood",
        link:"https://www.ifood.com.br/delivery/salvador-ba/dominos-pizza---brotas-brotas/"
